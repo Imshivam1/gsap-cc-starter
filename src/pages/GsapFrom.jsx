14b2ac25b0f94a2efd7b1b5003b6e1c1b1249278
Shivam Kumar Manjhi <113127627+Imshivam1@users.noreply.github.com>
2024-08-29 14:24:19 +0530

    Initial commit

--- src/pages/GsapFrom.jsx
+++ src/pages/GsapFrom.jsx
@@ -1,5 +1,18 @@
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react";
+
 const GsapFrom = () => {
   // TODO: Implement the gsap.from() method
+  useGSAP(() => {
+    gsap.from("#green-box", {
+      x: 250,
+      repeat: -1,
+      yoyo: true,
+      rotation: 360,
+      duration: 2,
+    ease: "elastic",
+    });
+  }, []);
 
   return (
     <main>
